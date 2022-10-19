18507530f6a51cf10131fbaa86dc8570a49fd96d
topsystem <admin@topsystem.cc>
2022-10-19 17:51:37 +0800

    :new: add pay filed

--- publishable/database/migrations/2016_01_01_000000_create_payment_table.php
+++ publishable/database/migrations/2016_01_01_000000_create_payment_table.php
@@ -17,9 +17,10 @@ class CreatePaymentTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->comment('用户id');
             $table->string('gateway')->comment('支付网关');
+            $table->string('app_id')->comment('商户id');
             $table->string('channel')->comment('支付通道');
             $table->char('currency',3)->default('CNY')->comment('货币代码');
-            $table->string('trade_no',64)->comment('第三方流水号');
+            $table->string('trade_no',64)->comment('流水号');
             $table->decimal('amount')->comment('充值金额');
             $table->tinyInteger('status')->comment('0:待支付，1:已支付，2:已取消');
             $table->string('description')->comment('订单描述');
@@ -30,6 +31,7 @@ class CreatePaymentTable extends Migration
         Schema::create('payment_order_extension', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->comment('用户id');
+            $table->string('app_id')->comment('商户id');
             $table->integer('payment_id')->comment('支付订单id');
             $table->text('request_body')->comment('请求消息体');
             $table->text('response_body')->comment('响应消息体');
